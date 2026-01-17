f08a33780f5a24efdff2f6223a028e7861be459b
young pump <tafadzwa.makaza@bakertilly.co.zw>
2026-01-17 13:57:50 +0200

    admin password change2

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LearningTaskViewSet
 from .views import register
+from django.urls import path
+from .views import create_admin
+
 
 
 router = DefaultRouter()
@@ -10,4 +13,5 @@ router.register(r'tasks', LearningTaskViewSet, basename='tasks')
 urlpatterns = [
     path('', include(router.urls)),
      path('register/', register),
+      path("create-admin/", create_admin),
 ]
