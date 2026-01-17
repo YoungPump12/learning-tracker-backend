7ca62f652aac7ada027a2a003fc3eb10e42f1b5b
young pump <tafadzwa.makaza@bakertilly.co.zw>
2026-01-17 13:45:48 +0200

    admin password change

--- scheduler/views.py
+++ scheduler/views.py
@@ -4,6 +4,8 @@ from .serializers import LearningTaskSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 class LearningTaskViewSet(viewsets.ModelViewSet):
     serializer_class = LearningTaskSerializer
@@ -22,3 +24,15 @@ def register(request):
         serializer.save()
         return Response({"message": "User created successfully"})
     return Response(serializer.errors, status=400)
+
+def create_admin(request):
+    if User.objects.filter(username="admin").exists():
+        return JsonResponse({"status": "admin already exists"})
+
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="Admin1234!"
+    )
+
+    return JsonResponse({"status": "admin created"})
